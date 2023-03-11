02c14e53875b05918fad4dd2658b767242be5812
luke-youngmin-cho <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-03-11 14:27:28 +0900

    CSharp - Interface, Collections
    1. Interface
    - Usage of interface
    - Interface vs abstract class
    - Virtual method table
    
    2. Collections
    - Created MyDynamicArray & Implemented Insert, Find, Remove algorithms.
    - Concept of time complexity.

--- CSharp/Abstraction/Program.cs
+++ CSharp/Abstraction/Program.cs
@@ -1,3 +1,56 @@
 ﻿using Abstraction;
 
-Creature creature = new Creature();
+Warrior warrior = new Warrior();
+Wizard wizard = new Wizard();
+Knight knight = new Knight();
+Slime slime = new Slime();
+Rat rat = new Rat();
+
+warrior.Breath();
+wizard.Breath();
+knight.Breath();
+slime.Breath();
+rat.Breath();
+warrior.Attack();
+
+// 자식타입은 부모타입으로 참조가 가능하다 
+Creature[] creatures = new Creature[5];
+creatures[0] = warrior;
+creatures[1] = wizard;
+creatures[2] = knight;
+creatures[3] = slime;
+creatures[4] = rat;
+//creatures[0].Attack();
+
+for (int i = 0; i < creatures.Length; i++)
+{
+    creatures[i].Breath();
+}
+
+for (int i = 0; i < creatures.Length; i++)
+{
+    // is 연산자 : 왼쪽의 객체가 오른쪽 타입으로 형변환을 시도하고 가능하면 true, 아니면 false를 반환
+    if (creatures[i] is IAttackable)
+    {
+        ((IAttackable)creatures[i]).Attack();
+    }
+}
+
+// nullable 형식 : null 값 할당이 가능하다고 명시하는 형식. 여기서 nullable 해준 이유는 as 연산시 null 반환되기 때문에..
+IAttackable? tmpAttackable;
+for (int i = 0; i < creatures.Length; i++)
+{
+    // as 연산자 : 왼쪽의 객체를 오른쪽타입으로 형변환을 시도하고 성공시 캐스팅된 참조를, 실패시 null 을 반환
+    tmpAttackable = creatures[i] as IAttackable;
+    if (tmpAttackable != null)
+    {
+        tmpAttackable.Attack();
+    }
+}
+
+
+IAttackable[] attackers = new IAttackable[4];
+attackers[0] = new Warrior();
+attackers[1] = new Wizard();
+attackers[2] = new Knight();
+attackers[3] = new Slime();
---
+++ CSharp/Abstraction/Warrior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    internal class Warrior : Character
+    {
+        int currentWeaponID; // 0 == low, 1 == proper, 2 == high
+        ISword sword;
+
+        new public void Attack()
+        {
+            sword.Attack();
+        }
+
+        public void Skill()
+        {
+            sword.Skill();
+        }
+    }
+}
+
+public interface ISword
+{
+    void Attack();
+    void Skill();
+}
+
+public interface ILowSword : ISword
+{
+}
+
+public interface IProperSword : ISword
+{
+}
+
+public interface IHighSword : ISword
+{
+}
